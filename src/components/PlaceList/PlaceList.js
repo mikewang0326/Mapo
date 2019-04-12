77741cf739bcd56aa089c513d28993971e3ebb97
MikeWang <hi.from.mikewang@gmail.com>
2019-04-13 09:55:19 +1200

    Add touch event

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import { View, StyleSheet} from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import ListItem from "../ListItem/ListItem"
 
 const placeList = props => {
     const placesOutput = props.places.map((place, i) => (
-        <ListItem key={i} placeName={place}/>)
-      );
-    
+        <ListItem
+            key={i}
+            placeName={place}
+            onItemPressed={() => alert("Item pressed - ID: " + i)} />)
+    );
+
     return (
         <View style={styles.listContainer}>
             {placesOutput}
@@ -15,9 +18,9 @@ const placeList = props => {
 };
 
 const styles = StyleSheet.create({
-    listContainer:{
-        width:"100%",
-      }
+    listContainer: {
+        width: "100%",
+    }
 });
 
 export default placeList;
