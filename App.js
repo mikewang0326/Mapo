17bb97d29c815a63b17224aea36188b4deed20fc
MikeWang <hi.from.mikewang@gmail.com>
2019-04-19 18:55:52 +1200

    Update to redux

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { Navigation } from 'react-native-navigation';
+import { Provider } from 'react-redux';
+
 import AuthScreen from './src/screens/Auth/Auth'
 import SharePlaceScreen from './src/screens/SharePlace/sharePlace'
 import FindPlaceScreen from './src/screens/FindPlace/findPlace'
 
+import configtureStore from './src/store/configureStore'
+
+const store = configtureStore();
+
+
 // Register Screens
-Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen);
-Navigation.registerComponent("awesome-places.SharePlaceScreen", () => SharePlaceScreen);
-Navigation.registerComponent("awesome-places.FindPlaceScreen", () => FindPlaceScreen);
+Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen, store, Provider);
+Navigation.registerComponent("awesome-places.SharePlaceScreen", () => SharePlaceScreen, store, Provider);
+Navigation.registerComponent("awesome-places.FindPlaceScreen", () => FindPlaceScreen, store, Provider);
 
 // Start a App
 Navigation.startSingleScreenApp({
--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,4 +1,4 @@
-import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE } from "./actionTypes"
+import { ADD_PLACE, DELETE_PLACE } from "./actionTypes"
 
 export const addPlace = (placeName) => {
     return {
@@ -14,15 +14,3 @@ export const deletePlace = (key) => {
     }
 }
 
-export const selectPlace = (key) => {
-    return {
-        type: SELECT_PLACE,
-        placeKey: key
-    }
-}
-
-export const deselectPlace = () => {
-    return {
-        type: DESELECT_PLACE
-    }
-}
