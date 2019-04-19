8795846d8005093c730c84063692479d993f3eab
MikeWang <hi.from.mikewang@gmail.com>
2019-04-19 18:12:19 +1200

    Add Basic Navigator

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import { Navigation } from 'react-native-navigation';
 import AuthScreen from './src/screens/Auth/Auth'
+import SharePlaceScreen from './src/screens/SharePlace/sharePlace'
+import FindPlaceScreen from './src/screens/FindPlace/findPlace'
 
 // Register Screens
 Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen);
+Navigation.registerComponent("awesome-places.SharePlaceScreen", () => SharePlaceScreen);
+Navigation.registerComponent("awesome-places.FindPlaceScreen", () => FindPlaceScreen);
 
 // Start a App
 Navigation.startSingleScreenApp({
